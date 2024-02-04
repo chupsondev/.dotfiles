225f214289dd15e8ff230d70ad5f13fbc2ff078d
chupson <chupson@chupson.dev>
2024-02-04 10:34:20 +0100

    install bufferline

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -102,6 +102,8 @@ require('lazy').setup({
 
     { 'wakatime/vim-wakatime', lazy = false },
 
+    {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+
     require 'chupson.plugins.debug',
 
 }, {})
@@ -474,3 +476,23 @@ cmp.setup {
         { name = 'path' },
     },
 }
+
+require("bufferline").setup{
+    options = {
+        numbers = "ordinal"
+    }
+}
+
+vim.keymap.set("n", "<leader>[b", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<leader>]b", ":BufferLineCyclePrev<CR>")
+
+vim.keymap.set("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>")
+vim.keymap.set("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>")
+vim.keymap.set("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>")
+vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>")
+vim.keymap.set("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>")
+vim.keymap.set("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>")
+vim.keymap.set("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>")
+vim.keymap.set("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>")
+vim.keymap.set("n", "<leader>$", ":BufferLineGoToBuffer -1<CR>")
