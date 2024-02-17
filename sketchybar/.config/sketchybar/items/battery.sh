05bd85034a91057cb58c4061fe1b259f870b6d7d
chupson <chupson@chupson.dev>
2024-02-17 20:49:26 +0100

    sketchybar: update battery periodicaly

--- sketchybar/.config/sketchybar/items/battery.sh
+++ sketchybar/.config/sketchybar/items/battery.sh
@@ -3,4 +3,5 @@
 sketchybar  --add item battery right                            \
             --set battery   script="$PLUGIN_DIR/battery.sh"     \
                             label.drawing=on                    \
+                            update_freq=200                     \
             --subscribe battery power_source_change system_woke \
