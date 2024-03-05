15c8f7b977269f5684adeb0ec87ce9d3e8967004
chupson <chupson@chupson.dev>
2024-03-05 18:18:48 +0100

    Add `<` autopairs rule to Neovim

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,14 @@ return {
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
-        opts = {}
+        config = function()
+            local npairs = require("nvim-autopairs")
+            local Rule = require('nvim-autopairs.rule')
+            npairs.setup({})
+
+            local cond = require('nvim-autopairs.conds')
+            npairs.add_rule(Rule("<", ">"):with_pair(cond.before_regex("[a-zA-Z0-9:<>]+")):with_move(function(opts) return
+                opts.char == ">" end))
+        end
     },
 }
