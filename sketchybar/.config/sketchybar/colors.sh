5986a2847219199d62bfebddbd1f1cbfd3076090
chupson <chupson@chupson.dev>
2024-03-05 21:29:33 +0100

    Change sketchybar theme to Gruvbox
    The theme of sketchybar is now Gruvbox. Might change it to kanagawa
    sometime but who knows.

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
+
 # Dracula
-export BLACK=0xff21222c
-export WHITE=0xfff8f8f2
-export RED=0xffff5555
-export GREEN=0xff50fa7b
-export BLUE=0xffbd93f9
-export YELLOW=0xfff1fa8c
-export ORANGE=0xffffb86c
-export MAGENTA=0xffFF79C6
-export GREY=0xff939ab7
-export PURPLE=0xffBD93F9
-export TRANSPARENT=0x00000000
-export BG0=0xff282A36
-export BG1=0x606272A4
-export BG2=0x6044475A
+# export BLACK=0xff21222c
+# export WHITE=0xfff8f8f2
+# export RED=0xffff5555
+# export GREEN=0xff50fa7b
+# export BLUE=0xffbd93f9
+# export YELLOW=0xfff1fa8c
+# export ORANGE=0xffffb86c
+# export MAGENTA=0xffFF79C6
+# export GREY=0xff939ab7
+# export PURPLE=0xffBD93F9
+# export TRANSPARENT=0x00000000
+# export BG0=0xff282A36
+# export BG1=0x606272A4
+# export BG2=0x6044475A
+
+# Gruvbox
+export BG0=0xff282828
+export BG1=0xff3c3836
+export WHITE=0xffebdbb2
 
 
 export BAR_COLOR=$BG0
