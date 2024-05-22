d355d6fead24e32665a14bf2a39a264acb0c2a74
chupson <chupson@chupson.dev>
2024-05-22 13:32:32 +0200

    Make Neovim transparent

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -59,7 +59,7 @@ return {
         priority = 1000,
         lazy = false,
         opts = {
-            styles = { transparency = false }
+            styles = { transparency = true }
         }
     },
     {
