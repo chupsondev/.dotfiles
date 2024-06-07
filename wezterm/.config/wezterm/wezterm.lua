44ff84344491aaecc1339c18bc5ed92c062867be
chupson <chupson@chupson.dev>
2024-06-07 06:55:32 +0200

    Revert "Implement toggling dark/light mode in terminal and nvim"
    This reverts commit fb781c007fb65d26452131b477c9906b72284cc5.
    
    I decided that this was kinda stupid and that I didn't really need it,
    so off it goes.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,10 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local function get_appearance()
-    local theme_file = os.getenv("HOME") .. "/.cache/theme"
-    local f = io.open(theme_file, "rb")
-    if f then f:close() end
-    if f == nil then
-        return 'dark'
-    end
-    local first_line = io.lines(theme_file)()
-    if first_line == 'dark' then
-        return 'dark'
-    elseif first_line == 'light' then
-        return 'light'
-    end
-    return 'dark'
-end
-
 local config = wezterm.config_builder()
 
 config.term = "wezterm"
-if get_appearance() == 'dark' then
-    config.color_scheme = "rose-pine"
-else
-    config.color_scheme = "rose-pine-dawn"
-end
+config.color_scheme = "rose-pine"
 config.font = wezterm.font_with_fallback {
     "FiraCode Nerd Font",
     "Symbols Nerd Font"
@@ -59,21 +39,6 @@ config.keys = {
     { key = 'Paste', mods = 'NONE',       action = act.PasteFrom "Clipboard" },
     { key = 'Copy',  mods = 'NONE',       action = act.CopyTo "Clipboard" },
     { key = 'f',     mods = 'SUPER',      action = act.ToggleFullScreen },
-    {
-        key = 't',
-        mods = 'SUPER',
-        action = wezterm.action_callback(function()
-            local current_theme = get_appearance()
-            local file = io.open(os.getenv("HOME").."/.cache/theme", "w")
-            if current_theme == 'dark' then
-                file:write("light")
-            else
-                file:write("dark")
-            end
-            file:close()
-            wezterm.reload_configuration()
-        end),
-    }
 }
 
 
