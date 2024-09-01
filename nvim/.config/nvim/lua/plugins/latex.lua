9f7cec59c9a199485254606d8e86e88e8c247f9d
chupson <chupson@chupson.dev>
2024-09-01 12:56:00 +0200

    Configure VimTex

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -1,5 +1,10 @@
 return {
     "lervag/vimtex",
+    lazy = false,
     init = function()
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_latexmk = { out_dir = "target" }
+        vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
+        vim.g.vimtex_quickfix_open_on_warning = 0
     end
 }
